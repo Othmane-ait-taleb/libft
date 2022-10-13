636c81673568e260512942780fcad2415ca644d2
Othmane-ait-taleb <96418976+Othmane-ait-taleb@users.noreply.github.com>
2022-10-13 19:18:46 +0100

    itoa just added

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 18:14:20 by otait-ta          #+#    #+#             */
-/*   Updated: 2022/10/13 19:15:22 by otait-ta         ###   ########.fr       */
+/*   Updated: 2022/10/13 19:18:19 by otait-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,28 +33,20 @@ char *ft_itoa(int n)
     int size;
     int i;
     char *rtr;
+
     i = 0;
-    
     size = ft_n_of_d(n);
-    rtr = malloc((size + 1)* sizeof(char));
+    rtr = (char *)malloc((size + 1)* sizeof(char));
     if (!rtr)
         return (NULL);
     rtr[size] = '\0';
     size--;
-
     while (size >= 0)
     {
-        rtr[size] = n % 10;
-
+        rtr[size] = 48 + (n % 10);
         size--;
-        n = n / 10 ;    
+        n = n / 10 ;
     }
-
     return rtr;
 }
 
-int main(int argc, char const *argv[])
-{
-    printf("%sdfgdfg",ft_itoa(848));
-    return 0;
-}
